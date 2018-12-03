cfc70da80f90e67e9ed7f5211fbe17da15050426
Josef <Josef.NymanMuller@yh.nackademin.se>
2018-12-03 01:43:42 +0100

    Merge origin/Develop into Develop
    Conflicts:
    	src/GameClient/ScoreScene.java

---
+++ src/Advent/Day2/Day2.java
@@ -0,0 +1,69 @@
+package Advent.Day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day2 {
+
+    static List<String> read() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/Advent/Day2/day2.txt"));
+        List<String> idList = new ArrayList<>();
+        while (sc.hasNext()) {
+            idList.add(sc.next());
+        }
+        return idList;
+    }
+
+    static int question1(List<String> ids) {
+        int twos = 0, threes = 0;
+        byte[] occur;
+        for (String id : ids) {
+            occur = new byte[id.length()];
+            for (int i = 0; i < id.length(); i++) {
+                occur[id.charAt(i) - 97]++;
+            }
+            boolean two = false, three = false;
+            for (byte b : occur) {
+                if (b == 2) {
+                    two = true;
+                } else if (b == 3) {
+                    three = true;
+                }
+            }
+
+            if (two) {
+                twos++;
+            }
+            if (three) {
+                threes++;
+            }
+        }
+        return twos * threes;
+
+    }
+
+    static String question2(List<String> ids) {
+        for (int i = 0; i < ids.size() - 1; i++) {
+            for (int j = i + 1; j < ids.size(); j++) {
+                for (int k = 0; k < ids.get(i).length(); k++) {
+                    if (ids.get(i).charAt(k) != ids.get(j).charAt(k)
+                            && ids.get(i).substring(k + 1)
+                                    .equals(ids.get(i).substring(k + 1))) {
+                        return ids.get(i).substring(0, k) + ids.get(i).substring(k + 1);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> ids = read();
+        System.out.println(question1(ids));
+        System.out.println(question2(ids));
+    }
+}
