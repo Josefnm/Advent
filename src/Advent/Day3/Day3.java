39b4d730ed6beee58aecf42681c6963ca27ae3e5
Josef <Josef.NymanMuller@yh.nackademin.se>
2018-12-03 16:43:40 +0100

    changed to bools

--- src/Advent/Day3/Day3.java
+++ src/Advent/Day3/Day3.java
@@ -25,31 +25,31 @@ public class Day3 {
         return cuts;
     }
 
-    static int[][] findOverlap(List<int[]> cuts) {
-        int[][] overlap = new int[1000][1000];
+    static Boolean[][] findOverlap(List<int[]> cuts) {
+        Boolean[][] overlap = new Boolean[1000][1000];
         cuts.forEach(cut -> {
             for (int i = 0; i < cut[3]; i++) {
                 for (int j = 0; j < cut[4]; j++) {
-                    overlap[i + cut[1]][j + cut[2]]++;
+                    overlap[cut[1]+i][cut[2]+j] = (overlap[cut[1]+i][cut[2]+j] != null);
                 }
             }
         });
         return overlap;
     }
 
-    static long question1(int[][] overlap) {
+    static long question1(Boolean[][] overlap) {
         return Arrays.stream(overlap)
-                .flatMapToInt(Arrays::stream)
-                .filter(i -> i > 1)
+                .flatMap(Arrays::stream)
+                .filter(i -> i != null && i)
                 .count();
     }
 
-    static int question2(int[][] overlap, List<int[]> cuts) {
+    static int question2(Boolean[][] overlap, List<int[]> cuts) {
         a:
         for (int[] cut : cuts) {
             for (int i = 0; i < cut[3]; i++) {
                 for (int j = 0; j < cut[4]; j++) {
-                    if (overlap[i + cut[1]][j + cut[2]] != 1) {
+                    if (overlap[i + cut[1]][j + cut[2]]) {
                         continue a;
                     }
                 }
@@ -61,7 +61,7 @@ public class Day3 {
 
     public static void main(String[] args) throws IOException {
         List<int[]> cuts = read();
-        int[][] overlap = findOverlap(cuts);
+        Boolean[][] overlap = findOverlap(cuts);
         System.out.println(question1(overlap));
         System.out.println(question2(overlap, cuts));
     }
